edb7a7c9085c5ec53e1bb67e73bdcea1a9e0e168
Trisha Moorkoth <tmoo2620@gmail.com>
2024-11-24 15:06:58 -0800

    working on offensive agent

--- pacai/student/myTeam.py
+++ pacai/student/myTeam.py
@@ -1,7 +1,8 @@
 from pacai.util import reflection
+from pacai.student.defensiveAgents import *
 
 def createTeam(firstIndex, secondIndex, isRed,
-        first = 'pacai.student.defensiveAgents.DefenseAgentDQN',
+        first = 'pacai.student.defensiveAgents.DummyAgent1',
         second = 'pacai.student.defensiveAgents.DummyAgent1'):
     """
     This function should return a list of two agents that will form the capture team,
@@ -10,8 +11,11 @@ def createTeam(firstIndex, secondIndex, isRed,
     and will be False if the blue team is being created.
     """
 
-    firstAgent = reflection.qualifiedImport(first)
-    secondAgent = reflection.qualifiedImport(second)
+    # firstAgent = reflection.qualifiedImport(first)
+    # secondAgent = reflection.qualifiedImport(second)
+
+    firstAgent = DummyAgent1
+    secondAgent = DummyAgent1
 
     return [
         firstAgent(firstIndex),
